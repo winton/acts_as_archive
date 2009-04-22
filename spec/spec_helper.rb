c74205d597bf794cd5af49e781e06d03847fcb2b
winton <mail@wintoni.us>
2009-04-22 00:00:26 -0700

    Spec reorganization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,23 +21,23 @@ end
 def establish_test_db
   # Establish connection
   unless ActiveRecord::Base.connected?
-    config = YAML::load(File.open("#{SPEC}/fixtures/database.yml"))['test']
+    config = YAML::load(File.open("#{SPEC}/config/database.yml"))['test']
     ActiveRecord::Base.establish_connection(config)
   end
   # Establish logger
-  logger_file = File.open("#{SPEC}/fixtures/test.log", 'a')
+  logger_file = File.open("#{SPEC}/log/test.log", 'a')
   logger_file.sync = true
   @logger = Logger.new logger_file
   ActiveRecord::Base.logger = @logger
   # Drop articles and archived_articles
   ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS articles")
   ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS archived_articles")
-  # Create articles
+  # Create articles table
   ActiveRecord::Base.connection.create_table(:articles) do |t|
     t.string :title
     t.string :body
     t.timestamps
   end
   # Load model
-  require "#{SPEC}/fixtures/article"
+  require "#{SPEC}/models/article"
 end
