ab0eb4648072782d081ada2655eeb84b93e24cc1
winton <mail@wintoni.us>
2010-02-03 17:49:08 -0800

    Adding success message to bin/acts_as_archive

--- lib/acts_as_archive/base.rb
+++ lib/acts_as_archive/base.rb
@@ -10,6 +10,7 @@ module ActsAsArchive
         klass.create_archive_table
         klass.migrate_from_acts_as_paranoid
         klass.create_archive_indexes
+        puts 'Success!'
       end
     end
   end
