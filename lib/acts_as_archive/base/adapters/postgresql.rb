26a1639b879a7537eafe43231e5544cae31a9553
Luke Francl <look@recursion.org>
2010-02-17 18:57:25 -0600

    refine query for getting indexes from postgresql

--- lib/acts_as_archive/base/adapters/postgresql.rb
+++ lib/acts_as_archive/base/adapters/postgresql.rb
@@ -2,30 +2,36 @@ module ActsAsArchive
   module Base
     module Adapters
       module PostgreSQL
-        
+
         private
 
         def archive_table_indexed_columns
+          # This query comes courtesy of cope360:
+          # http://stackoverflow.com/questions/2204058/show-which-columns-an-index-is-on-in-postgresql/2213199#2213199
           index_query = <<-SQL
-            SELECT c2.relname as index_name
-            FROM pg_catalog.pg_class c,
-              pg_catalog.pg_class c2,
-              pg_catalog.pg_index i
-            WHERE c.oid = (SELECT c.oid
-              FROM pg_catalog.pg_class c
-              WHERE c.relname ~ '^(archived_#{table_name})$')
-            AND c.oid = i.indrelid
-            AND i.indexrelid = c2.oid
-          SQL
-          
-          index_query.gsub!(/\s+/, ' ').strip!
-          indexes = connection.select_all(index_query).collect do |r|
-            r["index_name"]
-          end
+select
+    t.relname as table_name,
+    i.relname as index_name,
+    a.attname as column_name
+from
+    pg_class t,
+    pg_class i,
+    pg_index ix,
+    pg_attribute a
+where
+    t.oid = ix.indrelid
+    and i.oid = ix.indexrelid
+    and a.attrelid = t.oid
+    and a.attnum = ANY(ix.indkey)
+    and t.relkind = 'r'
+    and t.relname = 'archived_#{table_name}'
+order by
+    t.relname,
+    i.relname
+SQL
 
-          # HACK: reverse engineer the column name
-          indexes.map do |index|
-            index.split("_on_").last
+          indexes = connection.select_all(index_query).collect do |r|
+            r["column_name"]
           end
         end
       end
