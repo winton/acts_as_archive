bbf2ae980332ad965c4d4fb759ba23a5bc002ce7
winton <mail@wintoni.us>
2010-02-21 12:52:04 -0800

    Version 0.2.2 (Improved PostgreSQL support, now marking ActiveRecord objects as destroyed)

--- require.rb
+++ require.rb
@@ -17,7 +17,7 @@ Require do
     name 'acts_as_archive'
     homepage "http://github.com/winton/#{name}"
     summary "Don't delete your records, move them to a different table"
-    version '0.2.1'
+    version '0.2.2'
   end
   
   lib do
