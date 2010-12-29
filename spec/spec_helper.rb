efbfc27a73177763a74aff3a10fc2dcbfb33de93
winton <mail@wintoni.us>
2010-12-29 02:27:37 -0800

    Removing debug method in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,3 @@ $db, $log, $mail = ActiveWrapper.setup(
   :env => 'test'
 )
 $db.establish_connection
-
-# For use with rspec textmate bundle
-def debug(object)
-  puts "<pre>"
-  puts object.pretty_inspect.gsub('<', '&lt;').gsub('>', '&gt;')
-  puts "</pre>"
-end
